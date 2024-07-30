a97d0ca81563d00f0821c33130bf1e507551e101
Yeasin-Hossen-Arman <yeasinhossenarman@gmail.com>
2024-07-30 11:35:50 +0600

    add file

--- DeployVercel/settings.py
+++ DeployVercel/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = "DeployVercel.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
+# comment database for deploy on vercel
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
